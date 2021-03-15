6cf37ae4a4198df19225431e56550367a8353142
Oleg Kan <kan.fcsd@gmail.com>
2021-03-15 19:40:51 +0200

    add spinner. fixed issues

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const GIPHY_URL = 'https://api.giphy.com/v1/stickers/search?'; 
-const API_KEY = 'api_key=E26iZ0BIEszi7RA4GeEmXzJPRpkSeQ6m';
-const SEARCH_PARAM = '&q=';
-const AMOUNT = '&limit=50';
 
-let numberElements = 5;
-let userWord;
-let url;
-let json;
+const numberElements = 5;
 
-const input = document.getElementById('findInput');
-const imgWrap = document.querySelector('.wrap-for-copy').cloneNode(true);
-
-document.querySelector('.wrap-for-copy').remove();
-imgWrap.classList.remove('wrap-for-copy');
-
-const replaceImage = () => {
-  //shuffle array-------------
-  let array = [...json.data];
-  array.sort(function() {
-      return Math.random() - 0.5
-  });
-  //--------------------------
-  const needGifArr = array.slice(0, numberElements)
-  needGifArr.forEach(gif => {
-    const newImg = imgWrap.cloneNode(true);
-    newImg.querySelector('img').src = gif.images.original.url;
-    newImg.querySelector('img').alt = gif.title;
-    document.getElementById('listImages').appendChild(newImg);
-  })
+const getImageShell = () => {
+  const imgWrap = document.querySelector('.wrap-for-copy').cloneNode(true);
+  document.querySelector('.wrap-for-copy').remove();
+  imgWrap.classList.remove('wrap-for-copy');
+  return imgWrap;
 }
 
-const start = async function() {
-  let response = await fetch(url);
+const imgForCopy = getImageShell();
+
+const replaceImage = (serverArray, neededImg) => {
+  document.querySelector('.spinner').style.display = "none";
+  const array = [...serverArray]; //if I will need to add images on the screen I will need the full array
+  const needGifArr = array.slice(0, neededImg);
+  for (let i = 1; i <= neededImg; i++) {
+    const newImg = imgForCopy.cloneNode(true);
+    document.getElementById('listImages').appendChild(newImg);
+
+  }
+  const arrForChange = document.querySelectorAll('.image-wrap img');
+  for (let i = 0; i < arrForChange.length; i++) {
+    arrForChange[i].src = needGifArr[i].images.original.url;
+  }
+}
+
+const start = async(userWord) => {
+  const GIPHY_URL = 'https://api.giphy.com/v1/stickers/search?'; 
+  const API_KEY = 'E26iZ0BIEszi7RA4GeEmXzJPRpkSeQ6m';
+
+  const searchParams = new URLSearchParams();
+  searchParams.append('api_key', API_KEY);
+  searchParams.append('q', userWord);
+  const url = GIPHY_URL + searchParams;
+
+  const response = await fetch(url);
   if (response.ok) {
-    json = await response.json();
-    replaceImage();
+    const json = await response.json();
+    replaceImage(json.data, numberElements);
   } else {
     alert("ERROR HTTP: " + response.status);
   }
@@ -42,10 +45,8 @@ const start = async function() {
 
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  userWord = input.value.trim();
-  input.value = '';
+  const userWord = document.getElementById('findInput').value.trim();
+  start(userWord);
+  document.getElementById('findInput').value = '';
   document.getElementById('listImages').innerHTML = '';
-  url = GIPHY_URL + API_KEY + SEARCH_PARAM + userWord + AMOUNT;
-  console.log(url);
-  start();
 })
